935cb9d24b78a3624377a9c9c9af765060cf9bb1
pakdonghun <66273924+pakdonghun@users.noreply.github.com>
2021-01-15 22:53:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from flask import Flask
 
 app = Flask("SuperScrapper")
 
+@app.route("/")
+def home():
+  return "Hello! Welcome to mi casa!"
+
+@app.route("/contact")
+def pdh():
+  return "안녕하세요!"
+
 app.run(host="0.0.0.0")
