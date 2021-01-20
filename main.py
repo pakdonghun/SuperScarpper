1636beca53735dee7c3a55cfeff2a90b571dc451
pakdonghun <66273924+pakdonghun@users.noreply.github.com>
2021-01-20 22:52:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("SuperScrapper")
+
+db = {}
 
 @app.route("/")
 def home():
@@ -12,10 +15,29 @@ def report():
   word = request.args.get('word')
   if word:
     word = word.lower()
-    jobs = get_jobs(word)
-    print(jobs)
+    existinfJobs = db.get(word)
+    if existinfJobs:
+      jobs = existinfJobs
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
   else:
     return redirect("/")
-  return render_template("report.html", searchingBy=word)
+  return render_template("report.html",searchingBy=word,resultNumber=len(jobs),jobs=jobs
+  )
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get("word")
+    if not word:
+      raise Exception()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+  except:
+    return redirect("/")
 
 app.run(host="0.0.0.0")
